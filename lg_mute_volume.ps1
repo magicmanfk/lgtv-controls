f52d2a5edb9958002362782e084d12a493ce8ddf
tofu <your-username@users.noreply.github.com>
2025-06-17 21:37:01 -0500

    changed mute button device behavior

--- lg_mute_volume.ps1
+++ lg_mute_volume.ps1
@@ -1,12 +1,5 @@
 . C:\Users\media\code\lgtv-controls\lg_tv_functions.ps1
 
-# Remove everything here down except the last line if you don't want the toast notification when you unmute.
-$volumeObject = (& $LGTVcli -request com.webos.service.audio/master/getVolume)|ConvertFrom-json
-$currentlyMuted=$volumeObject.Device1.payload.volumeStatus.muteStatus
-
 & "C:\Program Files\LGTV Companion\LGTV Companion.exe" -button MUTE device1
-
-if ( $currentlyMuted ) {
-    activateVolume
-    displayVolume
-}
+activateVolume
+displayVolume
